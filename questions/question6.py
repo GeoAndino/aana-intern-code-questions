af74705566a964ceac6e020172d767695ae6c9f7
Geo-Andino <116525372+Geo-Andino@users.noreply.github.com>
2024-06-05 17:27:15 -0500

    completed algorithmic questions 1, 3, and python questions 5, 6

--- questions/question6.py
+++ questions/question6.py
@@ -1,5 +1,21 @@
 # Question 6
 # Python-Specific Questions:
-# Question: Implement a Python function that takes a string and returns a dictionary where the keys are the unique words in the string, and the values are the frequencies of each word.
+# Question: Implement a Python function that takes a string and 
+# returns a dictionary where the keys are the unique words in the string, 
+# and the values are the frequencies of each word.
 
 # Code solution here
+
+## create function called wordFrequencies which takes in a srtring
+
+def wordFrequencies(word) :
+## creates an empty dictionary and splits the string into words based on white space
+    uniqueDic = {}
+    words = word.split()
+## use for loop to iterate through the list of words and determine whether to increase the count by 1 or set it to default value of 1
+    for w in words: 
+        if ((uniqueDic.get(w, 0)) != 0) :
+            uniqueDic[w] += 1
+        elif () :
+            uniqueDic.setdefault(w, 1)
+    return uniqueDic
